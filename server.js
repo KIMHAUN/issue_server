9bc1e5dde09871a1b51688f32b0feed748b02044
user <soochan7@gmail.com>
2024-06-16 22:23:29 +1000

    Update Function

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.use(cors());
 
 app.use(express.urlencoded()); 
 
+app.use(express.json());
+
 app.listen(8080, () => {
     console.log('Running on http://localhost:8080')
 })
@@ -20,28 +22,28 @@ app.post('/createIssue', (req, res) => {
     const issue = req.body;
     issue.id = Math.random();
     issues.push(issue);
-    console.log('Created ::: ', issue);
+    //console.log('Created ::: ', issue);
     res.redirect('http://localhost:3000/');
 });
 
 //Read
-app.get('/', (요청, 응답) => {
-  응답.send(issues)
+app.get('/', (req,res) => {
+  res.send(issues)
 }) 
 
 // Update
-app.put('/issues/:id', (req, res) => {
-    const id = parseInt(req.params.id);
+app.post('/updateIssue', (req, res) => {
     const updatedIssue = req.body;
-    issues = issues.map(issue => (issue.id === id ? updatedIssue : issue));
-    console.log('Updated:', updatedIssue);
-    res.send(updatedIssue);
+   // console.log('Updated:', updatedIssue);
+    issues = issues.map(issue => issue.id == updatedIssue.id ? updatedIssue : issue);
+    
+    res.redirect('http://localhost:3000/');
 });
 
 // Delete
 app.get('/deleteIssue/:id', (req, res) => {
     const id = parseInt(req.params.id);
     issues = issues.filter(issue => issue.id !== id);
-    //console.log('Deleted issue ID:', id);
+    console.log('Deleted issue ID:', id);
     res.redirect('http://localhost:3000/');
 });
